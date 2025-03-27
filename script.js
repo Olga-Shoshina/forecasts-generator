41e62e427b6fbcc784f7a97abb376ea6c62e8dd3
Olga-Shoshina <taekvondo100@gmail.com>
2025-03-27 17:58:24 +0300

    генератор предсказаний
    добавила генератор предсказаний на страницу

--- script.js
+++ script.js
@@ -13,3 +13,58 @@
 /* При генерации нового предсказания старое предсказание должно добавляться в начало списка «Мои предсказания» — .forecasts  */
 
 /* Для добавления предсказания в список воспользуйся шаблоном forecast-item */
+
+const prediction1 = 'Сегодня вечером посвяти время на любовь к себе';
+const prediction2 = 'Ты на правильном пути, продолжай в том же духе';
+const prediction3 = 'Скоро ты получишь приятную новость';
+const prediction4 = 'Доверяй своей интуиции';
+const prediction5 = 'Не бойся трудностей, они делают тебя сильнее';
+
+const forecastButton = document.querySelector('.forecast-btn');
+const currentForecastTitle = document.querySelector('.current-forecast h1');
+const currentForecastText = document.querySelector('.current-forecast p');
+const forecastsContainer = document.querySelector('.forecasts');
+
+function generatePrediction() {
+    const randomNum = Math.floor(Math.random() * 5) + 1;
+    
+    let prediction;
+    switch (randomNum) {
+        case 1:
+            prediction = prediction1;
+            break;
+        case 2:
+            prediction = prediction2;
+            break;
+        case 3:
+            prediction = prediction3;
+            break;
+        case 4:
+            prediction = prediction4;
+            break;
+        case 5:
+            prediction = prediction5;
+            break;                   
+    }
+
+    const probability = Math.floor(Math.random() * 101);
+
+    currentForecastTitle.textContent = prediction;
+    currentForecastText.textContent = `Вероятность: ${probability}%`;
+
+    const newForecastItem = document.createElement('div');
+    newForecastItem.classList.add('forecast-item');
+
+    const forecastTitle = document.createElement('h3');
+    forecastTitle.textContent = prediction;
+
+    const forecastProbability = document.createElement('p');
+    forecastProbability.textContent = `Вероятность:  ${probability}%`;
+
+    newForecastItem.append(forecastTitle);
+    newForecastItem.append(forecastProbability);
+
+    forecastsContainer.prepend(newForecastItem);
+}
+
+    forecastButton.addEventListener('click', generatePrediction);
